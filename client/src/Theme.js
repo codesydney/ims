bd9a96e558373623d8b4ec5787af40c7cfca0992
deBubz <bao.es.hoang2@gmail.com>
2021-05-08 11:28:28 +1000

    client - setup basic global MUI theme

---
+++ client/src/Theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+/* 
+    Global Material UI theme
+*/
+
+const Theme = createMuiTheme({
+    spacing: 12,
+    typography: {
+        fontSize: 14,
+    },
+});
+
+export default Theme;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core";
+import Theme from "./Theme";
+
+import reportWebVitals from "./reportWebVitals";
+
+/* 
+  Top level of the react app
+  hook into DOM with the id root
+*/
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={Theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
